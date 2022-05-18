7412d654e604a59ef2b7ed2fcdfa8fc9fc6364cc
mauricio.barroso <dev.mauricioalezandre@gmail.com>
2022-05-18 07:57:44 -0300

    first commit

---
+++ z-body/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>z-Body</h1>
+    <button type="button" @click="goTo('/z-header')">CAD</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  methods: {
+    goTo(path){
+      console.log(path)
+
+      // dispatchEvent(new CustomEvent(
+      //   '@zitrus/z-body/goToRoute',
+      //   {
+      //     detail: {
+      //       path: path
+      //     }
+      //   }
+      // ))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #005f33;
+}
+</style>
---
+++ z-header/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav style="display: flex">
+    <router-link to="/z-header">Home</router-link>
+    <router-link to="/z-body">CAD</router-link>
+    <router-link to="/z-modulo-x">Modulo x</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 10px;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
